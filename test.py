3a217e2fcc3140caf56c7012ec97e2e24f84b622
Ali Hassan <alithephantom@gmail.com>
2016-10-16 22:31:06 +0200

    Add resize function

--- test.py
+++ test.py
@@ -32,16 +32,15 @@ def resize(imagePath, width, height):
     xPos, yPos = 0, 0
     while xPos < width: #Loop through rows
         while yPos < height: #Loop through collumns
-            valR = sourceImage.item(int(yPos / height * sourceImage.shape[0]), \
-                                    int(xPos / width * sourceImage.shape[1]),2)
-            valG = sourceImage.item(int(yPos / height * sourceImage.shape[0]), \
-                                    int(xPos / width * sourceImage.shape[1]),1)
-            valB = sourceImage.item(int(yPos / height * sourceImage.shape[0]), \
-                                    int(xPos / width * sourceImage.shape[1]),0)
-            print(val, int(yPos / height * sourceImage.shape[0]), int(xPos / width * sourceImage.shape[1]))
-            blankImage.itemset((yPos, xPos, 0), val) #Set B to val
-            blankImage.itemset((yPos, xPos, 1), val) #Set G to val
-            blankImage.itemset((yPos, xPos, 2), val) #Set R to val
+            valR = sourceImage.item(int((yPos*1.0) / height * sourceImage.shape[0]), \
+                                    int((xPos*1.0) / width * sourceImage.shape[1]),2)
+            valG = sourceImage.item(int((yPos*1.0) / height * sourceImage.shape[0]), \
+                                    int((xPos*1.0) / width * sourceImage.shape[1]),1)
+            valB = sourceImage.item(int((yPos*1.0) / height * sourceImage.shape[0]), \
+                                    int((xPos*1.0) / width * sourceImage.shape[1]),0)
+            blankImage.itemset((yPos, xPos, 0), valB) #Set B to val
+            blankImage.itemset((yPos, xPos, 1), valG) #Set G to val
+            blankImage.itemset((yPos, xPos, 2), valR) #Set R to val
 
             yPos = yPos + 1 #Increment Y position by 1
 
@@ -55,6 +54,6 @@ def resize(imagePath, width, height):
 # threashold("images/L1.jpg", "images/L1_output.jpg", 2**20)
 # cv2.imshow("opencv",cv2.imread("images/L1_output.jpg"))
 
-resize("images/logo.jpg", 669.0, 325.0)
+resize("images/logo.jpg", 669, 325)
 
 cv2.waitKey(0)
